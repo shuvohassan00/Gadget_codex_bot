797a364941d22c21a38aadcdd3c5e678e612167f
Shuvo Hassan <155814909+shuvohassan00@users.noreply.github.com>
2026-02-18 04:27:26 +0600

    Change API_ID, API_HASH, BOT_TOKEN, and URL
    Updated API credentials and channel URL.

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 #Copyright @ISmartCoder
 #Updates Channel @abirxdhackz
-API_ID = 15xxxxx
-API_HASH = 'e04ab312a57ex'
-BOT_TOKEN = 'xxx:xxx-c8zhgjm8G3YbOM8A'
-UPDATE_CHANNEL_URL = 't.me/abirxdhackz'
+API_ID = 35944528
+API_HASH = '67754b8c8c0c459831a117030eb6faf5'
+BOT_TOKEN = '8472500254:AAGA4Y9GjVv_lhNaxZq5idt-sdOiLQmBG5A'
+UPDATE_CHANNEL_URL = 'https://t.me/gadgetpremiumzone'
 
 COMMAND_PREFIXES = ['/', '!', '.', ',', '$', '#']
 
